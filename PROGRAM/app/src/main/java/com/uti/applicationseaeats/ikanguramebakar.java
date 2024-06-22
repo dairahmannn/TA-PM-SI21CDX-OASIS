f5c0380a8252ef1ff80f7ca1177364c3afe9f4fd
Muhammadyudha30 <myudha299@gmail.com>
2024-06-23 02:02:48 +0700

    yudha: membuat kotlin+java dengan nama ikanguramebakar

---
+++ PROGRAM/app/src/main/java/com/uti/applicationseaeats/ikanguramebakar.java
@@ -0,0 +1,14 @@
+package com.uti.applicationseaeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ikanguramebakar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ikan_gurame);
+    }
+}
