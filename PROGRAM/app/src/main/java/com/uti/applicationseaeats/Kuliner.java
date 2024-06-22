fbcd15aff9dbf08027a5e42ed96da223dd33f2b0
Muhammadyudha30 <myudha299@gmail.com>
2024-06-23 01:52:14 +0700

    yudha: membuat new string di kotlin+java Kuliner guna untuk mengisi teks pada menu

--- PROGRAM/app/src/main/java/com/uti/applicationseaeats/Kuliner.java
+++ PROGRAM/app/src/main/java/com/uti/applicationseaeats/Kuliner.java
@@ -1,4 +1,48 @@
 package com.uti.applicationseaeats;
 
-public class Kuliner {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Kuliner extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuliner);
+
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[]{"Ikan Gurame Bakar","Ikan Laut Bakar Dan Goreng","BestSeller SeaFood Kepiting Dan Udang"
+
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), ikanguramebakar.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), ikanlaut.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), streetfood.class);
+                    startActivityForResult(myIntent, 0);
+
+                }
+            });
+        }
+    }
