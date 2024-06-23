6746c0c49e519d13e80ef06dc8a765249e7991a7
dairahmannn <dairahmanbakti18@gmail.com>
2024-06-23 09:51:33 +0700

    master: Mengdeklarasikan di mainactivity

--- PROGRAM/app/src/main/java/com/uti/applicationseaeats/MainActivity.kt
+++ PROGRAM/app/src/main/java/com/uti/applicationseaeats/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.uti.applicationseaeats
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var button1: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        button1 = findViewById<View>(R.id.button1) as Button
+
+        button1!!.setOnClickListener { v: View? ->
+            val intent1 = Intent(
+                applicationContext,
+                MenuUtama::class.java
+            )
+            startActivity(intent1)
         }
     }
 }
